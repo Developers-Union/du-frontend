15d8dbf032c476b0252f5476dfaa1e8f664b2cb6
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-07-12 08:54:42 +0800

    Add new

---
+++ pages/Personal Homepage/canvas.js
@@ -0,0 +1,71 @@
+var cvs = document.getElementById('charts');
+var ctx = cvs.getContext('2d');
+var dataList = [3.053,3.062,3.047,3.047,3.045];
+var preYAxisLst= ['3.045','3.05','3.055','3.06','3.065','3.07'];
+var yAxisLst = preYAxisLst.reverse();
+var xAxisList = ['周一','周二','周三','周四','周五']
+var intervalLine = 25;
+var lineWid = 270;
+var intervalRow = (0.8 * lineWid) / (xAxisList.length - 1);
+var xAxis = (preYAxisLst.length - 1) * intervalLine + 20;
+
+var valToCoor = function(val){
+    var deg = (preYAxisLst[preYAxisLst.length - 1] * 1000 - preYAxisLst[0] * 1000) / 1000;
+    var dec = (preYAxisLst[preYAxisLst.length - 1] * 1000 - val * 1000) / 1000;
+    var orgVal = intervalLine * (preYAxisLst.length - 1);
+    var pec = orgVal / deg;
+    var finalVal = orgVal - dec * pec + 20;
+    return finalVal;
+}
+
+
+cvs.height = preYAxisLst.length * intervalLine + 20
+ctx.beginPath();
+ctx.font = '10px Arial';
+ctx.fillStyle = 'rgb(110,110,110)';
+for(var i = 0; i < yAxisLst.length; i++){
+    ctx.moveTo(0, 20 + i * intervalLine),
+    ctx.lineTo(lineWid,  20 + i * intervalLine);
+    ctx.fillText(yAxisLst[i], lineWid + 5, 24 + i * intervalLine)
+}
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'rgb(221,221,221)';
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.font = '11px Arial'
+ctx.fillStyle = 'rgb(110,110,110)';
+for(var j = 0; j < xAxisList.length; j++){
+    ctx.moveTo(intervalRow * j, xAxis),
+    ctx.lineTo(intervalRow * j, xAxis + 10);
+    ctx.fillText(xAxisList[j], 0 + intervalRow * j, xAxis + 22)
+}
+ctx.lineWidth = 1.5;
+ctx.strokeStyle = 'rgb(201,213,226)';
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(2, valToCoor(dataList[0]));
+for(var l = 1; l < dataList.length; l++){
+    ctx.lineTo(intervalRow * l, valToCoor(dataList[l]));
+}
+ctx.strokeStyle = 'rgb(200,66,67)';
+ctx.stroke();
+
+for(var k = 0; k < dataList.length; k++){
+    var xVal = intervalRow * k;
+    var yVal =  valToCoor(dataList[k])
+    if(k == 0){
+        xVal = 4;
+    }
+    ctx.beginPath();
+    ctx.arc(xVal, yVal, 3, 0, 2*Math.PI)
+    ctx.fillStyle = 'rgb(200,66,67)';
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(xVal, yVal, 2, 0, 2*Math.PI)
+    ctx.fillStyle = 'white';
+    ctx.fill();
+}
